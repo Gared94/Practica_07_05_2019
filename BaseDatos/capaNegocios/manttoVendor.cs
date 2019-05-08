7d675342ce6bbd0e9fac9b799fe035f8812015ee
Gared94 <47989107+Gared94@users.noreply.github.com>
2019-05-07 20:06:04 -0600

    prctica 7 de Mayo de 2019

---
+++ BaseDatos/capaNegocios/manttoVendor.cs
@@ -0,0 +1,89 @@
+﻿using capaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaNegocios
+{
+    public class manttoVendor
+    {
+        bdConex test = new bdConex();
+        private string codVendor, nombreVendor;
+
+        public string CodVendor
+        {
+            get { return codVendor; }
+            set { codVendor = value; }
+        }
+
+        public string NombreVendor
+        {
+            get { return nombreVendor; }
+            set { nombreVendor = value; }
+        }
+
+
+        public void insertVendor()
+        {
+            try
+            {
+                String insert = "INSERT INTO VENDEDOR (VENDEDOR, NOMBRE)VALUES('"+codVendor+"','"+nombreVendor+"')";
+                test.Conectar();
+                test.consultarSQL(insert);
+                test.Desconectar();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public void updateVendor()
+        {
+            try
+            {
+                String update = "UPDATE VENDEDOR set NOMBRE ='" + nombreVendor + "' WHERE VENDEDOR ='" + codVendor + "'";
+                test.Conectar();
+                test.consultarSQL(update);
+                test.Desconectar();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public void selectVendor()
+        {
+            try
+            {
+                String select = "SELECT * FROM VENDEDOR='" + codVendor + "'";
+                test.Conectar();
+                test.consultarSQL(select);
+                test.Desconectar();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public String prueba() 
+        {
+            String mensaje;
+
+            if(test.Conectar() == true)
+            {
+                test.Desconectar();
+                mensaje = "Conexion realizada";
+            }else
+            {
+                test.Desconectar();
+                mensaje = "Conexion no realizada";
+            }
+            return mensaje;
+        }
+    }
+}
---
+++ BaseDatos/capaPresentacion/frmVendor.cs
@@ -0,0 +1,55 @@
+﻿using capaNegocios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace capaPresentacion
+{
+    public partial class frmVendor : Form
+    {
+
+        manttoVendor obj = new manttoVendor();
+
+        public frmVendor()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVendor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(obj.prueba());
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            obj.CodVendor = txtCodigoVen.Text;
+            obj.NombreVendor = txtNombreVen.Text;
+            obj.insertVendor();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            obj.CodVendor = txtCodigoVen.Text;
+            obj.NombreVendor = txtNombreVen.Text;
+            obj.updateVendor();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtCodigoVen.Text =obj.CodVendor;
+            txtNombreVen.Text =obj.NombreVendor;
+            obj.selectVendor();
+        }
+    }
+}
